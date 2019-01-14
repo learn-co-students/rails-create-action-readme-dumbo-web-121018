a8a033fd978b0cdd492ae832c6f7423f746d129d
jisung594 <jisung594@gmail.com>
2019-01-14 09:37:40 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+p1 = Post.create(title: "Title of Blog Post", description: "This is the description.")
+p2 = Post.create(title: "Git this, Git that", description: "Intro for beginners.")
+p3 = Post.create(title: "Chicago's Best", description: "All about deep-dish pizza.")
+p4 = Post.create(title: "Blog Post Title", description: "Here is the fucking description.")
+p5 = Post.create(title: "Jian Yang's Adventures", description: "His life is a whirlwind.")
+p6 = Post.create(title: "Tintin's Startups", description: "Somali pirates, on the run.")
